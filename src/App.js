9da8f124ff03102852b78b8d06626d9bde145a40
HAYA <Hayaalsherouqi@gmail.com>
2023-04-07 14:17:02 +0300

    nav bar and signin/signup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './componants/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar, Form, DropdownButton, Dropdown, Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar style={{ backgroundColor: "#208075", fontFamily: "Verdana", fontWeight: "bold" }} variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#venue">Venue</Nav.Link>
+            <Nav.Link href="#decoration">Decoration</Nav.Link>
+            <Nav.Link href="#hospitality">Hospitality</Nav.Link>
+            <Nav.Link href="#dj">DJ</Nav.Link>
+            <Nav.Link href="#florist">Florist</Nav.Link>
+          </Nav>
+
+          <Button style={{ backgroundColor: "#7EABA6", fontFamily: "Arial", borderColor: "#208075" }} variant="dark" className="mx-2" >SIGN UP</Button>
+          <Button style={{ backgroundColor: "#7EABA6", fontFamily: "Arial", borderColor: "#208075" }} variant="dark" className="mx-2" >LOG IN </Button>
+
+        </Container>
+      </Navbar>
+    </>
   );
 }
+
 
 export default App;
