754174639b8c0889183ae96dcb9bfc02bdbb4f15
HAYA <Hayaalsherouqi@gmail.com>
2023-04-11 02:18:40 +0300

    vanue& dj

--- src/components/Buffet/buffet.js
+++ src/components/Buffet/buffet.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, ListGroup } from 'react-bootstrap';
+import { BsPlus } from 'react-icons/bs';
+
 
 export default function Buffet(props) {
   return (
     <div>
-        <td>{props.restaurantName}</td>
-        <td>{props.type}</td>
-        <td>{props.description}</td>
-        <td>{props.noOfGuests}</td>
-        <td>{props.price}</td>
-        <td><img src={props.image} ></img></td>
+  <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.restaurantName}</Card.Title>
+        <Card.Text>
+        {props.description}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Type: {props.type}</ListGroup.Item>
+        <ListGroup.Item>Number of Guests: {props.noOfGuests}</ListGroup.Item>
+        <ListGroup.Item>Price: {props.price} BD</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Text>
+          <Card.Link href="#" className="ml-auto">
+            <BsPlus /> 
+          </Card.Link>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
     </div>
-  )
+  );
 }
--- src/components/Hall/hall.js
+++ src/components/Hall/hall.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
 
 export default function Hall(props) {
   return (
     <div>
-        <td>{props.name}</td>
-        <td>{props.size}</td>
-        <td>{props.availableDate}</td>
-        <td>{props.price}</td>
-        <td><img src={props.image} ></img></td>        
+
+    <Card className="bg-dark text-white">
+      <Card.Img src="https://cdn0.weddingwire.ca/vendor/2943/3_2/960/jpg/fsmontreal-ballroom-night-v1_50_52943-157142093782072.jpeg" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+        {props.price}
+        </Card.Text>
+        <Card.Text>{props.size}</Card.Text>
+      </Card.ImgOverlay>
+      <Card.Footer className="text-muted">{props.availableDate}</Card.Footer>
+    </Card>     
     </div>
   )
 }
