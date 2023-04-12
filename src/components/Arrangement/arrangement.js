5dc4bf2427f2ccd3c44a59ef4c2b413945d09af4
emanali <emanali.12.1996@gmail.com>
2023-04-12 11:21:12 +0300

    fix

--- src/components/Arrangement/arrangement.js
+++ src/components/Arrangement/arrangement.js
@@ -10,9 +10,9 @@ export default function Arrangement(props) {
   <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={props.image} />
       <Card.Body>
-        <Card.Title>{props.description}</Card.Title>
+        <Card.Title>{props.type}</Card.Title>
         <Card.Text>
-        {props.type}
+        {props.description}
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
